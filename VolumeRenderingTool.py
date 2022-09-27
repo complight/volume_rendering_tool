b796330582656d6ac25b16ef2ec596040b2af695
Serhat Aksoy <aksoyse19@itu.edu.tr>
2022-09-27 10:09:33 +0100

    Update VolumeRenderingTool.py

--- VolumeRenderingTool.py
+++ VolumeRenderingTool.py
@@ -15,7 +15,7 @@ import bpy
 
 bpy.context.scene.render.engine = 'CYCLES'
 
-def create_2d_3d_converter(image_path,color,number):
+def create_2d_3d_converter(image_path,color,number,default):
     new_color = [0,0,0,1]
     for i in range(len(color)):
          new_color[i] = color[i] 
@@ -58,6 +58,8 @@ def create_2d_3d_converter(image_path,color,number):
     Contrast_node.location = (300,0)
     Emission = new_mat.node_tree.nodes.new(type = 'ShaderNodeEmission')    
     new_mat.node_tree.nodes["Emission"].inputs[0].default_value = new_color
+    if default == True:
+        new_mat.node_tree.links.new(texImage.outputs[0], Emission.inputs[0])
     Emission.location = (500,0)
     ReRoute1 = new_mat.node_tree.nodes.new(type = 'NodeReroute')
     ReRoute1.location = (-500,-400)
@@ -107,8 +109,11 @@ class MainPanel(bpy.types.Panel):
         row = layout.row()
         layout.prop(scene, 'my_color')
         row = layout.row()
+        layout.prop(scene, 'my_def')
+        row = layout.row()
         layout.prop(scene, 'my_int')
         row = layout.row()
+        
         row.operator('camera.locked_operator', icon = 'CUBE')
         row = layout.row()
         row.operator('camera.follow_operator', icon = 'CUBE')
@@ -150,9 +155,10 @@ class CREATE_OT_MATERIAL(bpy.types.Operator):
         layout = self.layout
         scene = context.scene
         color = bpy.context.scene.my_color
+        default = bpy.context.scene.my_def
         image_path = bpy.context.scene.my_path
         num =  bpy.context.scene.my_int
-        new_mat = create_2d_3d_converter(image_path,color,num)
+        new_mat = create_2d_3d_converter(image_path,color,num,default)
         mat = list(bpy.data.materials)[len(list(bpy.data.materials))-1]
         if len(list(bpy.data.objects['Cube'].data.materials)) > 0 :
             bpy.data.objects['Cube'].data.materials[0] = mat
@@ -165,6 +171,7 @@ def register():
     bpy.types.Scene.my_path = bpy.props.StringProperty(name="File", subtype='FILE_PATH')
     bpy.types.Scene.my_color = bpy.props.FloatVectorProperty(name ="Color",subtype = 'COLOR_GAMMA', default = (1,1,1))
     bpy.types.Scene.my_int = bpy.props.IntProperty(name = 'Image Number:')
+    bpy.types.Scene.my_def = bpy.props.BoolProperty(name = 'Default Color')
     bpy.utils.register_class(MainPanel)
     bpy.utils.register_class(CAMERA_OT_LOCKED)
     bpy.utils.register_class(CAMERA_OT_FOLLOW)
